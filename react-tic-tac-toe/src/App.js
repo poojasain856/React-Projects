c2f408ff895d70a753e8d3b80e51295d82e2ee1e
poojasain856 <poojasain856@gmail.com>
2023-10-13 23:14:08 +0530

    react files uploaded

---
+++ react-tic-tac-toe/src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    const squares = [...board];
+
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
+
+    squares[index] = xIsNext ? 'X' : 'O';
+    setBoard(squares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (board.every((square) => square)) {
+    status = 'Game over - It\'s a draw!';
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setXIsNext(true);
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+        {winner || board.every((square) => square) ? (
+          <button className="start-over-button" onClick={resetGame}>
+            Start Over
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+export default App;
